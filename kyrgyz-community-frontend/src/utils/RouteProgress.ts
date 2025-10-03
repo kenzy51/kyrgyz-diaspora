834f9e42f173549b4a6949602a94ae2d1b3783f5
Nazarov Kanat <nazarovkanat7@gmail.com>
2025-10-03 17:05:10 -0400

    fixed nprogress

--- kyrgyz-community-frontend/src/utils/RouteProgress.ts
+++ kyrgyz-community-frontend/src/utils/RouteProgress.ts
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import NProgress from "nprogress";
 import "nprogress/nprogress.css";
+import NProgress from "nprogress";
 
 export default function RouteProgress() {
   const location = useLocation();
